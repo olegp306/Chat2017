0a4fe4ad648598db7e7be5a2cec0fa4223b73031
olegp306 <olegp306@gmail.com>
2017-05-19 11:11:10 +0600

    сделал каркас, разбил на слои, добавил nunit

---
+++ Chat2017/Chat2017.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat2017.Tests
+{
+    public class Class1
+    {
+    }
+}
