d7b21e90c8e45c6da56ce55b9b01784cf59ee783
olegp306 <olegp306@gmail.com>
2017-05-23 18:37:27 +0600

    add data models

---
+++ Chat2017/Chat2017.Dal/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat2017.Dal.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public Chat Chat { get; set; }
+        public User User { get; set; }
+    }
+}
